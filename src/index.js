984d84d53da82b27fa4569f2d98bf49d98c6ca43
Alan4101 <andriy410tolochkevitch@gmail.com>
2019-04-06 01:49:13 +0300

    slider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 'use strict';
 import './scss/main.scss';
+import './js/slider';
 // import _ from 'lodash';
 
 (function() {
-    console.log(__dirname+'src/fonts/beyno');
+    let navigatioHover = e =>{
+
+    };
+    document.querySelector()
+    let obj = document.querySelector('.slider');
+    let o = obj.getBoundingClientRect();
+    // console.log(o.width);
+    // console.log(__dirname+'src/fonts/beyno');
 })();
