61642a8e8c9112029c5ca4abf7ef9bb82cae1c08
Alan4101 <andriy410tolochkevitch@gmail.com>
2019-04-06 02:17:51 +0300

    main slider js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './js/slider';
 // import _ from 'lodash';
 
 (function() {
-    let navigatioHover = e =>{
-
-    };
-    document.querySelector()
+    // let navigatioHover = e =>{
+    //
+    // };
+    // document.querySelector()
     let obj = document.querySelector('.slider');
     let o = obj.getBoundingClientRect();
     // console.log(o.width);
