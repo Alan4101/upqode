fede0b76e6fc45fcc5a27008e7b77eb3bc58a7a9
Alan4101 <andriy410tolochkevitch@gmail.com>
2019-04-15 16:36:01 +0300

    hover

--- src/index.js
+++ src/index.js
@@ -3,8 +3,37 @@ import './scss/main.scss';
 import './js/slider';
 // import _ from 'lodash';
 
+let d        = document,
+    navList  = d.querySelector('.nav__item'),
+    dropItem = d.querySelectorAll('.dropdown-nav__item');
+
+if(!Element.prototype.remove){
+    Element.prototype.remove = function remove(){
+        if(this.parentNode){
+            this.parentNode.removeChild(this);
+        }
+    }
+}
+
+dropItem.addEventListener('mouseenter', (e)=>{
+    // var el = e.target;
+    // console.log(el);
+    //
+    if(!dropItem.classList.contains('hover')){
+        dropItem.insertAdjacentHTML('afterBegin', `<i class="fas fa-chevron-right"></i>`);
+        dropItem.classList.add('hover');
+    }else {
+        dropItem.children[0].remove();
+        dropItem.classList.remove('hover');
+    }
+}, false);
+
+dropItem.addEventListener('mouseleave', ()=>{
+
+}, false);
 
 (function() {
+
     let progress =  document.querySelectorAll('progress');
     let percent = [79, 90, 69, 92];
 
